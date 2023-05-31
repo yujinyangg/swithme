43defa88a76c7f68e3cef278afbbeb3086dac09d
22yujin96 <134987890+22yujin96@users.noreply.github.com>
2023-05-31 16:29:23 +0900

    마이페이지 메인 매핑 -이유진-
    마이페이지 메인 매핑 -이유진-

--- swithMeProject/src/main/java/com/kh/swithme/member/controller/MemberControllerY.java
+++ swithMeProject/src/main/java/com/kh/swithme/member/controller/MemberControllerY.java
@@ -10,6 +10,11 @@ public class MemberControllerY {
 
 	private MemberServiceImpl memberService;
 	
+	@RequestMapping("mypage.me")
+	public String myPage() {
+		return "member/myPageMain";
+	}
+	
 	@RequestMapping("item.me")
 	public String myItemListView() {
 		return "member/myItem";
